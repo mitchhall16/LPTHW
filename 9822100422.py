dfbec791440032ffa2a9c0ad6a9834417364aad5
mitchhall16 <mitchhall16@gmail.com>
2022-10-04 16:52:16 -0700

    Add files via upload

---
+++ 9822100422.py
@@ -0,0 +1,39 @@
+print("Started LPTHW this past week and put a few hours on there so far.")
+print("Wooo! You've just run Mitch's Python Journey")
+print("Right now, I'm thinking the layout of this will be a basic overview of what I learned during each session!")
+print("So already when making & replaying this (print) is lowercase, got it")
+
+print("I'm all setup on Python3.10.6 - I'm already used to typing that phrase into the Terminal.")
+print("Todays date is 9/8/2022 @ 5:39PM this will be session 3 working with the hashtags or (#) - octothorpe and ex3.py")
+print("Mind whoever is reading this I'm of course typing every python exercise")
+print("Wooo we learninnn maff signs now 6:06PM - if you couldn't tell ex2.py was using the print function")
+print("Was just messing around with the Modulus, I'm familiar with this (Modelo) what I knew from JavaScript but just seemed odd to me? Weird but woo")
+
+maybe_operational_tractor_number = "PS8"
+state_of_charge = "78%"
+final_charge_of_PS8 = "54%"
+
+print("Time = 6:58PM, 9/8/22" "'Lets find the SOC% of a PS8'")
+print("If I drive", maybe_operational_tractor_number, "for 4 hours, and I leave containers with SOC at", state_of_charge)
+print("For each hour it drops 6% in battery lets do some math")
+print("X =", state_of_charge, "- 4(6)")
+print("X = 78 - 24")
+print("X = 54 or ", final_charge_of_PS8)
+print("At the end of the day", maybe_operational_tractor_number, "is at", state_of_charge)
+print("End of log 7:14PM - also really need to learn how to put line spaces in.")
+print("Going to rewrite ex3.py with float numbers just to get more familiar with numbers on keyboard.")
+print("Just found out I can put emoji's in Atom my Text Editor & it will show up in Terminal 💯 ")
+print("Already looking at the ex4.py exmample covering variables and this looks way simplier than JavaScript")
+
+my_name = "Mitchell"
+my_age = 26
+lots_of_words = "about variables, going back to some of the other code to spice it up"
+noise_1 = "!!!yeet "
+noise_2 = "and woo!!!"
+noise_3 = noise_1 + noise_2
+print("My name is", my_name, "and I'm", my_age, "years old!")
+print("Now that I learned", lots_of_words, noise_3)
+time_ending_session_sept8_2022 = "8:03PM"
+print("Okay time to call it for tonight I think at", time_ending_session_sept8_2022)
+current_day_October_fourth_twentytwenty = "10/04/2022"
+print("Okay back on the grind stone now that I'm day shift right now", current_day_October_fourth_twentytwenty)
